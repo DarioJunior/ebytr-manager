d54cdf648b2367cb3ecfe1b9b6eff589f961c662
Dário Junior <dario.junior.rs@gmail.com>
2021-11-02 15:53:05 -0300

    feat: BE: /tasks created post route, controller, service and model FE: created api service to connect with /tasks, !no input validations yet

--- backend/services/task.services.js
+++ backend/services/task.services.js
@@ -1,7 +1,6 @@
 const TaskModel = require('../models/task.models');
 
 async function addTask(taskInfo) {
-  console.log(taskInfo);
   const addictedTask = await TaskModel.addTaskInDB(taskInfo);
   return addictedTask;
 }
--- frontend/src/components/AddTaskModal/index.jsx
+++ frontend/src/components/AddTaskModal/index.jsx
@@ -1,20 +1,40 @@
 import PropTypes from 'prop-types';
-import { React } from 'react';
+import { React, useState } from 'react';
+
+import { addTask } from '../../api/task.services';
 
 import Modal from 'react-bootstrap/Modal';
 import ModalBody from 'react-bootstrap/ModalBody';
 import ModalHeader from 'react-bootstrap/ModalHeader';
 import ModalFooter from 'react-bootstrap/ModalFooter';
 import ModalTitle from 'react-bootstrap/ModalTitle';
-
 import { ContainerBody, Input, Select, TextArea, Button } from './styles';
 
 export default function AddTaskModal(props) {
   const { onHide } = props;
+  const [taskNameValue, setTaskNameValue] = useState('');
+  const [taskDescriptionValue, setTaskDescriptionValue] = useState('');
+  const [taskDateValue, setTaskDateValue] = useState('');
+  const [taskStatusValue, setTaskStatusValue] = useState('');
 
   async function handleSubmitForm(event) {
     event.preventDefault();
+
+    const success = await addTask(
+      taskNameValue,
+      taskDescriptionValue,
+      taskDateValue,
+      taskStatusValue,
+    );
+
+    console.log(success);
+
+    if (success) {
+      return onHide();
+    }
+    return alert('Revise as informações e tente novamente.');
   }
+
   return (
     <Modal
       { ...props }
@@ -31,20 +51,30 @@ export default function AddTaskModal(props) {
             name="taskName"
             type="text"
             placeholder="Add task..."
+            value={ taskNameValue }
+            onChange={ ({ target }) => setTaskNameValue(target.value) }
             required
           />
           <TextArea
             name="taskDescription"
             type="text"
             placeholder="Add description..."
+            value={ taskDescriptionValue }
+            onChange={ ({ target }) => setTaskDescriptionValue(target.value) }
             required
           />
           <Input
             name="taskDate"
             type="date"
+            onChange={ ({ target }) => setTaskDateValue(target.value) }
             required
           />
-          <Select name="taskStatus" id="taskStatus" required>
+          <Select
+            name="taskStatus"
+            value={ taskStatusValue }
+            onChange={ ({ target }) => setTaskStatusValue(target.value) }
+            required
+          >
             <option value="" hidden>
               Status
             </option>
