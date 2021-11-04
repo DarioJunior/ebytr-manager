d366683fb48d2bac75ce291f62248430fcb4f19b
Dário Junior <dario.junior.rs@gmail.com>
2021-11-04 17:57:31 -0300

    feat: add Login tests

--- frontend/src/tests/pages/login.test.js
+++ frontend/src/tests/pages/login.test.js
@@ -1,4 +1,5 @@
 import { React } from 'react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../../utils/renderWithRouter';
 import App from '../../App';
 
@@ -6,13 +7,37 @@ describe('1) LOGIN TESTs:', () => {
   it('1.1) Verify if input email exists', () => {
     const { getByTestId } = renderWithRouter(<App />);
     const emailInput = getByTestId('input-email');
-    const passwordInput = getByTestId('password-email');
-
 
     expect(emailInput).toBeInTheDocument();
-    expect(passwordInput).toBeInTheDocument();
+  });
 
-    // expect(history.length).toBe(2);
-    // expect(history.location.pathname).toBe('/about');
+  it('1.2) Verify if input password exists', () => {
+    const { getByTestId } = renderWithRouter(<App />);
+    const passwordInput = getByTestId('input-password');
+    expect(passwordInput).toBeInTheDocument();
+  });
+
+  it('1.3) Verify if login button exists', () => {
+    const { getByTestId } = renderWithRouter(<App />);
+    const loginButton = getByTestId('login-button');
+    expect(loginButton).toBeInTheDocument();
+  });
+
+  it('1.4) Verify if login button is disabled', () => {
+    const { getByTestId } = renderWithRouter(<App />);
+    const loginButton = getByTestId('login-button');
+    expect(loginButton).toBeDisabled();
+  });
+
+  it('1.4) Verify if login button is enable if infos placed', () => {
+    const { getByTestId } = renderWithRouter(<App />);
+    const emailInput = getByTestId('input-email');
+    const passwordInput = getByTestId('input-password');
+    const loginButton = getByTestId('login-button');
+    userEvent.type(emailInput, 'admin@example.com');
+    expect(emailInput.value).toBe('admin@example.com');
+    userEvent.type(passwordInput, 'admin12345');
+    expect(passwordInput.value).toBe('admin12345');
+    expect(loginButton).not.toBeDisabled();
   });
 });
