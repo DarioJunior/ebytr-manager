3beaa04f64c78b1885d1efb3e6eba2e58171c168
Dário Junior <dario.junior.rs@gmail.com>
2021-11-05 12:59:34 -0300

    refact text input color (red changed to black)

--- frontend/src/components/GenericInput/styles.js
+++ frontend/src/components/GenericInput/styles.js
@@ -1,5 +1,19 @@
 import styled from 'styled-components';
 
+// eslint-disable-next-line import/prefer-default-export
 export const Input = styled.input`
-  color: red;
+  @media screen and (min-width: 200px) {
+    min-width: 200px;
+    width: 85vw;
+    font-size: 1rem;
+  }
+
+  @media screen and (min-width: 575px) {
+    width: 465px;
+
+  }
+  
+  @media screen and (min-width: 992px) {
+    width: 765px;
+  } 
 `;
